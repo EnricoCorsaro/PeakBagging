780f5badddb26951b209a59cd0e88b721b1fcd36
Enrico Corsaro <enrico.corsaro@ster.kuleuven.be>
2013-09-25 17:27:15 +0200

    First general improvement and update of RegularPatternModel. Now it is supposed to fit all p modes.

--- include/RegularPatternModel.h
+++ include/RegularPatternModel.h
@@ -20,33 +20,41 @@ class RegularPatternModel : public Model
 {
     public:
     
-        RegularPatternModel(const RefArrayXd covariates, const int NradialOrders);
+        RegularPatternModel(const RefArrayXd covariates, const vector<int> &NparametersPerType, double nuMax);
         ~RegularPatternModel();
 
-        virtual void predict(RefArrayXd predictions, const RefArrayXd modelParameters);
-        int getNparameters();
-       
+        virtual void predict(RefArrayXd predictions, RefArrayXd const modelParameters);
+        double predictDeltaNuFromNuMax();
+        double predictDeltaNu01FromDeltaNu(double DeltaNu);
+        double predictDeltaNu02FromDeltaNu(double DeltaNu);
+        double predictDeltaNu03FromDeltaNu(double DeltaNu);
+
 
     protected:
         
         double computeSinglePmodeFrequency(int relativeRadialOrder, double frequencyOfCentralRadialMode, 
-                                           double DeltaNu, double deltaNu02, double epsilon, int angularDegree);
-        void computePmodeFrequencies(vector<int> relativeRadialOrders, double frequencyOfCentralRadialMode, 
-                                            double DeltaNu, double deltaNu02);
+                                           double DeltaNu, double deltaNu02, double deltaNu01, int angularDegree);
         // void computeMixedDipoleModeFrequencies(double frequencyOfCentralRadialMode, double DeltaNu, double DeltaPi1,
-        //                                        vector<int> relativeRadialOrders, double couplingFactor){};
+        //                                        double couplingFactor){};
 
 
 
     private:
 
-        int NradialOrders;                              // The number of radial orders of the model
-        int Nparameters;                                // The number of free parameters of the model
-        int radialOrderOfCentralRadialMode;             // The radial order of the central radial mode
-        vector<double> frequenciesOfPmodes;
-        vector<double> angularDegreesOfPmodes;
-        vector<double> radialOrdersOfPmodes;
-   
+        int NglobalParameters;                          // Total number of global parameters to be fitted 
+                                                        // (e.g. nuMax, white noise, period spacing, etc.)
+        int NprofileParameters;                         // Number of parameters determining the shape 
+                                                        // of the mode profile (height, linewidth, inclination angle, etc.)
+                                                        // N.B the mode frequency may not be considered as a profile parameter
+        int NradialOrders;                              // Number of radial orders expected in the PSD
+        int NangularDegrees;                            // Maximum number of angular degrees to be fitted
+        int NpressureModes;                             // Total number of pressure modes to be fitted 
+                                                        // N.B this number can differ from the product NangularDegrees*NradialOrders
+        int NmixedModes;                                // Total number of mixed modes to be fitted
+        double nuMax;                                   // The frequency of maximum power excess
+        vector<int> relativeRadialOrders;               // Array of integers containing the radial order numbers with respect to
+                                                        // the central one
+
 
 }; // END class RegularPatternModel
 
--- source/RegularPatternModel.cpp
+++ source/RegularPatternModel.cpp
@@ -7,15 +7,39 @@
 //      Constructor. Initializes model computation.
 //
 // INPUT:
-//      covariates:     one-dimensional array containing the values
-//                      of the independent variable.
-//      NradialOrders:        the number of radial orders to be fitted
+//      covariates:             one-dimensional array containing the values
+//                              of the independent variable.
+//      NparametersPerType:     configuring numbers for the Peak Bagging model
+//      nuMax:                  the frequency of maximum power excess
 //
 
-RegularPatternModel::RegularPatternModel(const RefArrayXd covariates, const int NradialOrders)
+RegularPatternModel::RegularPatternModel(const RefArrayXd covariates, const vector<int> &NparametersPerType, double nuMax)
 : Model(covariates),
-  NradialOrders(NradialOrders)
+  NglobalParameters(NparametersPerType[0]),
+  NprofileParameters(NparametersPerType[1]),
+  NradialOrders(NparametersPerType[2]),
+  NangularDegrees(NparametersPerType[3]),
+  NpressureModes(NparametersPerType[2]*NparametersPerType[3]),
+  NmixedModes(NparametersPerType[4]),
+  nuMax(nuMax)
 {
+    relativeRadialOrders.resize(NradialOrders);
+    int startingRadialOrder = 0;
+
+    if (static_cast<int>(fmod(NradialOrders,2)) == 0)
+    {
+        startingRadialOrder = -((NradialOrders/2) - 1);
+    }
+    else 
+        if ((static_cast<int>(fmod(NradialOrders,2)) != 0) && (NradialOrders != 1))
+        {
+            startingRadialOrder = -((NradialOrders - 1)/2);
+        }
+
+    for (int radialOrder = 0; radialOrder < NradialOrders; ++radialOrder)
+    {
+        relativeRadialOrders[radialOrder] = startingRadialOrder + radialOrder;
+    }
 
 }
 
@@ -60,90 +84,58 @@ RegularPatternModel::~RegularPatternModel()
 //      predictions:        one-dimensional array to contain the predictions
 //                          from the model
 //      modelParameters:    one-dimensional array where each element
-//                          contains the value of one free parameter of the model
+//                          contains the value of a free parameter of the model
 //
 // OUTPUT:
 //      void
 //
 // NOTE:
 //      The free parameters are to be given in the order
-//      (1) Frequency of central radial order
-//      (2) Large frequency separation (DeltaNu)
-//      (3) Small frequency separation 02 (deltaNu02)
-//      (4) White noise background (noiseLevel)
+//      (1) Reference Frequency of central radial order
+//      (2) White noise background (flat noise level)
+//      (3) Large frequency separation (DeltaNu)
+//      (4) Small frequency separation 02 (deltaNu02)
+//      (5) Small frequency separation 01 (deltaNu01)
+//      (5) Mode profile heights
+//      (6) Mode profile linewidths
 
-void RegularPatternModel::predict(RefArrayXd predictions, const RefArrayXd modelParameters)
+void RegularPatternModel::predict(RefArrayXd predictions, RefArrayXd const modelParameters)
 {
     Nparameters = modelParameters.size();
+    ArrayXd pressureModePrediction = ArrayXd::Zero(covariates.size());
+    //double linewidth = 0.15;
+    cout << modelParameters << endl;
 
-    double frequencyOfCentralRadialMode = modelParameters(0);
-    double DeltaNu = modelParameters(1);
-    double deltaNu02 = modelParameters(2);
-    double noiseLevel = modelParameters(3);
-    
-    vector<int> relativeRadialOrders(NradialOrders);
-    ArrayXd modes = ArrayXd::Zero(predictions.size());
-    
-    double height = 16.0;
-    double linewidth = 0.4;
-
-    radialOrderOfCentralRadialMode = floor(frequencyOfCentralRadialMode / DeltaNu);
-    double epsilon = frequencyOfCentralRadialMode / DeltaNu - fmod(frequencyOfCentralRadialMode, DeltaNu);
-    
-    for (int i=0; i < relativeRadialOrders.size(); ++i)
+    for (int radialOrder = 0; radialOrder < NradialOrders; ++radialOrder)
     {
-        relativeRadialOrders[i] = -static_cast<int>(floor(NradialOrders/2.0)) + i;
-
-        for (int j=0; j < 4; ++j)
+        for (int angularDegree = 0; angularDegree < NangularDegrees; ++angularDegree)
         {
-            double frequencyOfPmode = computeSinglePmodeFrequency(relativeRadialOrders[i], frequencyOfCentralRadialMode,
-                                                                  DeltaNu, deltaNu02, epsilon, j);
-            if (frequencyOfPmode >= covariates.minCoeff() && frequencyOfPmode <= covariates.maxCoeff())
+            double frequencyOfPmode = computeSinglePmodeFrequency(relativeRadialOrders[radialOrder], modelParameters(0),
+                                                                  modelParameters(NglobalParameters + radialOrder), 
+                                                                  modelParameters(NglobalParameters + NradialOrders + radialOrder),
+                                                                  modelParameters(NglobalParameters + 2*NradialOrders + radialOrder),
+                                                                  angularDegree);
+            
+            if ((frequencyOfPmode > covariates.minCoeff()) && (frequencyOfPmode < covariates.maxCoeff()))
             {
-                Functions::modeProfile(modes, covariates, frequencyOfPmode, height, linewidth);
-                predictions += modes;
-
+                Functions::modeProfile(pressureModePrediction, covariates, frequencyOfPmode, 
+                                       modelParameters(NglobalParameters + 3*NradialOrders + radialOrder + angularDegree), 
+                                       modelParameters(NglobalParameters + 3*NradialOrders + NpressureModes + 
+                                       radialOrder + angularDegree));
+                                       //linewidth);
+                predictions += pressureModePrediction;
             }
             else
+            {
                 continue;
+            }
         }
     }
     
-    //computePmodeFrequencies(relativeRadialOrders, frequencyOfCentralRadialMode, DeltaNu, deltaNu02);
-
-
-    /*
-    for (int i = 0; i < frequenciesOfPmodes.size(); i++)
-    {
-        Functions::modeProfile(modes, covariates, frequenciesOfPmodes[i], height, linewidth);
-        predictions += modes;
-    }
-    */
-    
-    predictions += noiseLevel;
+    predictions += modelParameters(1);           // Add flat noise level component
 
 }
 
-
-
-
-
-
-
-// RegularPatternModel::getNparameters()
-//
-// PURPOSE: 
-//      Get the private data member Nparameters;
-//
-// OUTPUT:
-//      Returns an integer containing the total number of 
-//      free parameters used in the model.
-//
-
-int RegularPatternModel::getNparameters()
-{
-    return Nparameters;
-}
 
 
 
@@ -166,9 +158,10 @@ int RegularPatternModel::getNparameters()
 //                                      order to be considered, expressed as
 //                                      the difference with respect to the
 //                                      radial order of the central radial mode
-//      frequencyOfCentralRadialMode:   the frequency corresponding to the central radial mode
+//      referenceFrequency:             the frequency corresponding to the central radial mode
 //      DeltaNu:                        the large frequency separation of p modes
 //      deltaNu02:                      the small frequency separation 02 of p modes
+//      deltaNu01:                      the small frequency separation 01 of p modes
 //      epsilon:                        the phase shift constant
 //      angularDegree:                  the angular degree of the mode
 //
@@ -176,49 +169,43 @@ int RegularPatternModel::getNparameters()
 //      A double containing the frequency of the desidered mode.
 //
 
-double RegularPatternModel::computeSinglePmodeFrequency(int relativeRadialOrder, double frequencyOfCentralRadialMode, 
-                                                        double DeltaNu, double deltaNu02, double epsilon, int angularDegree)
+double RegularPatternModel::computeSinglePmodeFrequency(int relativeRadialOrder, double referenceFrequency, double DeltaNu, 
+                                                        double deltaNu02, double deltaNu01, int angularDegree)
 {
-    
     // Compute the alpha factor used for the curvature correction
-    
-    double alphaCorrection = 0.015 * pow(DeltaNu,-0.32);        // Mosser et al. 2012, A&A, 548, A10
+    // double alphaCorrection = 0.015 * pow(DeltaNu,-0.32);        // Mosser et al. 2012, A&A, 548, A10
    
 
-    // Compute the constant d0l to be used in the asymptotic relation.
-    // The values are derived from the univeral pattern proposed by Mosser et al. 2011, A&A, 525, L9
-
-    double constantDell = 0.0;
-
+    // Compute the first order term of the Tassoul's asymptotic relation in units of DeltaNu
+    
+    double linearTerm = 0.0;
+    double startingFrequency = nuMax + referenceFrequency;
+    
     switch (angularDegree)
     {
-        case 0:
-        constantDell = 0.0;
+        case 0:        
+        linearTerm = DeltaNu*relativeRadialOrder + startingFrequency;
         break;
-        
+    
         case 1:
-        constantDell = -0.056 + (-0.002)*log(DeltaNu);
+        linearTerm = DeltaNu*(relativeRadialOrder + 0.5) + startingFrequency - deltaNu01;
         break;
 
         case 2:
-        constantDell = deltaNu02/DeltaNu;
+        linearTerm = DeltaNu*(relativeRadialOrder) + startingFrequency - deltaNu02;
         break;
 
         case 3:
-        constantDell = 0.280;
+        linearTerm = DeltaNu*(relativeRadialOrder + 0.5) + startingFrequency - 0.280*DeltaNu;
         break;
     }
 
-    // Compute the first order term of the Tassoul's asymptotic relation in units of DeltaNu
-
-    double linearTerm = relativeRadialOrder + radialOrderOfCentralRadialMode + epsilon + angularDegree/2.0 - constantDell;
-   
 
     // Compute the second order term of the Tassoul's asymptotic relation in units of DeltaNu
-    
-    double quadraticTerm = alphaCorrection/2.0 * relativeRadialOrder * relativeRadialOrder;
+    // double quadraticTerm = alphaCorrection/2.0 * relativeRadialOrder * relativeRadialOrder;
+    // return DeltaNu * (linearTerm + quadraticTerm);
 
-    return DeltaNu * (linearTerm + quadraticTerm);
+    return linearTerm;
 }
 
 
@@ -230,47 +217,113 @@ double RegularPatternModel::computeSinglePmodeFrequency(int relativeRadialOrder,
 
 
 
-
-
-// RegularPatternModel::computePmodeFrequencies()
+// RegularPatternModel::predictDeltaNuFromNuMax()
 //
 // PURPOSE:
-//      Computes the frequency of an entire set of p modes 
-//      based on the Tassoul's asymptotic relation. The formalism
-//      adopted is that used by Mosser et al. 2012, A&A, 540, A143 and
-//      Mosser et al. 2012, A&A, 548, A10
-//
+//      Predict the average DeltaNu based on nuMax by means of
+//      the scaling relation revised by Huber et al. (2011), ApJ 743, 143
+//      for the subset of Kepler Red Giants.
+//      
 // INPUT:
-//      relativeRadialOrders:           a vector of integers specifying the radial
-//                                      orders to be considered, expressed as
-//                                      the difference with respect to the
-//                                      radial order of the central radial mode.
-//      frequencyOfCentralRadialMode:   the frequency corresponding to the central radial mode
-//      DeltaNu:                        the large frequency separation of p modes
-//      deltaNu02:                      the small frequency separation 02 of p modes
 //
 // OUTPUT:
-//      The output frequencies, angular degress and radial orders are stored into vectors.
+//      A double containing the value of DeltaNu 
 //
 
-void RegularPatternModel::computePmodeFrequencies(vector<int> relativeRadialOrders, double frequencyOfCentralRadialMode, 
-                                                         double DeltaNu, double deltaNu02)
+double RegularPatternModel::predictDeltaNuFromNuMax()
 {
-
-    double epsilon = frequencyOfCentralRadialMode / DeltaNu - fmod(frequencyOfCentralRadialMode, DeltaNu);
-    radialOrderOfCentralRadialMode = floor(frequencyOfCentralRadialMode / DeltaNu);
-
-    for (int i = 0; i < relativeRadialOrders.size(); ++i)
-    {
-        for (int angularDegree = 0; angularDegree < 4; ++angularDegree)
-        {
-            double frequencyOfPmode = frequencyOfCentralRadialMode + DeltaNu/2.0 - deltaNu02; 
-            // computeSinglePmodeFrequency(relativeRadialOrders[i], frequencyOfCentralRadialMode,
-            //                                                      DeltaNu, deltaNu02, epsilon, angularDegree);
-            frequenciesOfPmodes.push_back(frequencyOfPmode);
-            angularDegreesOfPmodes.push_back(angularDegree);
-            radialOrdersOfPmodes.push_back(relativeRadialOrders[i]+radialOrderOfCentralRadialMode);
-        }
-    }
+ double alpha = 0.267;
+ double beta = 0.760;
+ 
+ return alpha*pow(nuMax,beta);
 }
 
+
+
+
+
+
+
+
+
+
+
+// RegularPatternModel::predictDeltaNu01FromDeltaNu()
+//
+// PURPOSE:
+//      Predict the average deltaNu01 based on DeltaNu by means of
+//      the scaling relation revised by Mosser et al. (2011), A&A 525, 9
+//      
+// INPUT:
+//      DeltaNu:    a double specifying the DeltaNu to be used
+//
+// OUTPUT:
+//      A double containing the value of deltaNu01 
+//
+
+double RegularPatternModel::predictDeltaNu01FromDeltaNu(double DeltaNu)
+{
+ double A = -0.056;
+ double B = -0.002;
+ 
+ return DeltaNu*(A + B*log10(DeltaNu));
+}
+
+
+
+
+
+
+
+
+
+
+// RegularPatternModel::predictDeltaNu02FromDeltaNu()
+//
+// PURPOSE:
+//      Predict the average deltaNu02 based on DeltaNu by means of
+//      the scaling relation revised by Mosser et al. (2011), A&A 525, 9
+//      
+// INPUT:
+//      DeltaNu:    a double specifying the DeltaNu to be used
+//
+// OUTPUT:
+//      A double containing the value of deltaNu02 
+//
+
+double RegularPatternModel::predictDeltaNu02FromDeltaNu(double DeltaNu)
+{
+ double A = 0.131;
+ double B = -0.033;
+ 
+ return DeltaNu*(A + B*log10(DeltaNu));
+}
+
+
+
+
+
+
+
+
+
+
+// RegularPatternModel::predictDeltaNu03FromDeltaNu()
+//
+// PURPOSE:
+//      Predict the average deltaNu03 based on DeltaNu by means of
+//      the scaling relation revised by Mosser et al. (2011), A&A 525, 9
+//      
+// INPUT:
+//      DeltaNu:    a double specifying the DeltaNu to be used
+//
+// OUTPUT:
+//      A double containing the value of deltaNu03 
+//
+
+double RegularPatternModel::predictDeltaNu03FromDeltaNu(double DeltaNu)
+{
+ double A = 0.280;
+ 
+ return DeltaNu*A;
+}
