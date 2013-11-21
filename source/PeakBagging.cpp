55e5d88529af05186eb36e79005cd35327f28a23
Enrico Corsaro <enrico.corsaro@ster.kuleuven.be>
2013-11-21 23:05:56 +0100

    Corrected PeakBagging main according to new feature of BackgroundModel.

--- source/PeakBagging.cpp
+++ source/PeakBagging.cpp
@@ -18,12 +18,17 @@
 #include "NormalPrior.h"
 #include "ExponentialLikelihood.h"
 #include "LorentzianMixtureModel.h"
+#include "CanBackgroundModel.h"
 #include "FerozReducer.h"
 #include "Results.h"
 #include "Ellipsoid.h"
 
 int main(int argc, char *argv[])
 {
+    // ---------------------------
+    // ----- Read input data -----
+    // ---------------------------
+
     unsigned long Nrows;
     int Ncols;
     ArrayXXd data;
@@ -58,44 +63,48 @@ int main(int argc, char *argv[])
     ArrayXd observations = data.col(1);
    
    
-    // Zero step - Set up problem dimensions
+    // --------------------------------------------------
+    // ----- Zeroth step. Set up problem dimensions -----
+    // --------------------------------------------------
 
-    vector<int> NparametersPerType(5);                              // Vector containing the configuring parameters for the LorentzianMixture model
-    int NglobalParameters = 1;
-    NparametersPerType[0] = NglobalParameters;                      // Total number of global parameters to be fitted 
-                                                                    // (e.g. referenceFrequency for central radial order, flat noise level, 
-                                                                    // period spacing, etc.)
+    vector<int> NparametersPerType(2);                              // Vector containing the configuring parameters for the LorentzianMixture model
     int NprofileParameters = 3;
-    NparametersPerType[1] = NprofileParameters;                     // Number of parameters determining the shape 
+    NparametersPerType[0] = NprofileParameters;                     // Number of parameters determining the shape 
                                                                     // of the mode profile (central frequency, height, linewidth, inclination angle, etc.)
-    int Nmodes = 6; 
-    NparametersPerType[2] = Nmodes;                                 // Total number of modes to fit
+    int Nmodes = 2; 
+    NparametersPerType[1] = Nmodes;                                 // Total number of modes to fit
+    int Ndimensions = NprofileParameters*Nmodes;
+    cout << "--------------------------------------" << endl;
+    cout << "Inference problem has " << Ndimensions << " dimensions." << endl;
+    cout << "--------------------------------------" << endl;
     
     
-    // First step - Set up a model for the inference problem
+    // -------------------------------------------------------------------
+    // ----- First step. Set up the models for the inference problem ----- 
+    // -------------------------------------------------------------------
     
-    LorentzianMixtureModel model(covariates, NparametersPerType);
+    // Chose a model for background and configure it, then do the same for the peakbagging model.
+    
+    CanBackgroundModel backgroundModel(covariates);
+    string outputDirName(argv[2]);
+    string backgroundConfiguringParameters = outputDirName + "CanModel_configuringParameters.txt";
+    backgroundModel.readConfiguringParametersFromFile(backgroundConfiguringParameters);
+    LorentzianMixtureModel model(covariates, NparametersPerType, backgroundModel);
 
 
-    // Second step - Setting Prior distribution
+    // -------------------------------------------------------
+    // ----- Second step. Set up all prior distributions -----
+    // -------------------------------------------------------
     
+    // Total number of prior types
+
     vector<Prior*> ptrPriors(1);
-    
-
-    // Total number of free parameters (dimensions) of the problem
-
-    int Ndimensions = NglobalParameters + NprofileParameters*Nmodes;
 
 
     // Uniform Prior
 
     ArrayXd parametersMinima(Ndimensions);                      // Minima
     ArrayXd parametersMaxima(Ndimensions);                      // Maxima
-    
-    double noiseLevelMinimum = 7;                               // Flat noise level
-    double noiseLevelMaximum = 11.0;
-    parametersMinima(0) = noiseLevelMinimum;
-    parametersMaxima(0) = noiseLevelMaximum;
     
     ArrayXd centralFrequencyMinima(Nmodes);                     // Central frequency of oscillation mode
     ArrayXd naturalLogarithmOfHeightsMinima(Nmodes);            // Natural logarithm of mode height
@@ -104,27 +113,22 @@ int main(int argc, char *argv[])
     ArrayXd naturalLogarithmOfHeightsMaxima(Nmodes);            // Natural logarithm of mode height
     ArrayXd linewidthsMaxima(Nmodes);                           // Mode Linewidth (FWHM)
 
-//  centralFrequencyMinima(0) = 142.5;
-//  centralFrequencyMaxima(0) = 144.0;
-//  naturalLogarithmOfHeightsMinima.fill(4.6);
-//  naturalLogarithmOfHeightsMaxima.fill(8.01);
-//  linewidthsMinima.fill(0.01);
-//  linewidthsMaxima.fill(0.5);
     
-    centralFrequencyMinima(0) = 119.3;
-    centralFrequencyMaxima(0) = 120.3;
-    naturalLogarithmOfHeightsMinima(0) = 3.4;
-    naturalLogarithmOfHeightsMaxima(0) = 4.9;
-    linewidthsMinima(0) = 0.01;
-    linewidthsMaxima(0) = 0.3;
+    centralFrequencyMinima(0) = 119.5;
+    centralFrequencyMaxima(0) = 120.0;
+    naturalLogarithmOfHeightsMinima(0) = 5.5;
+    naturalLogarithmOfHeightsMaxima(0) = 6.2;
+    linewidthsMinima(0) = 0.2;
+    linewidthsMaxima(0) = 0.4;
     
+
     centralFrequencyMinima(1) = 120.4;
     centralFrequencyMaxima(1) = 122.5;
-    naturalLogarithmOfHeightsMinima(1) = 5.6;
+    naturalLogarithmOfHeightsMinima(1) = 5.8;
     naturalLogarithmOfHeightsMaxima(1) = 6.9;
-    linewidthsMinima(1) = 0.2;
-    linewidthsMaxima(1) = 0.45;
-    
+    linewidthsMinima(1) = 0.28;
+    linewidthsMaxima(1) = 0.46;
+/*    
     centralFrequencyMinima(2) = 126.8;
     centralFrequencyMaxima(2) = 127.3;
     naturalLogarithmOfHeightsMinima(2) = 3.6;
@@ -153,92 +157,36 @@ int main(int argc, char *argv[])
     linewidthsMinima(5) = 0.05;
     linewidthsMaxima(5) = 0.3;
 
-    parametersMinima.segment(NglobalParameters, Nmodes) = centralFrequencyMinima;
-    parametersMinima.segment(NglobalParameters + Nmodes, Nmodes) = naturalLogarithmOfHeightsMinima;
-    parametersMinima.segment(NglobalParameters + 2*Nmodes, Nmodes) = linewidthsMinima;
-    parametersMaxima.segment(NglobalParameters, Nmodes) = centralFrequencyMaxima;
-    parametersMaxima.segment(NglobalParameters + Nmodes, Nmodes) = naturalLogarithmOfHeightsMaxima;
-    parametersMaxima.segment(NglobalParameters + 2*Nmodes, Nmodes) = linewidthsMaxima;
+    centralFrequencyMinima(0) = 142.5;
+    centralFrequencyMaxima(0) = 144.0;
+    naturalLogarithmOfHeightsMinima.fill(4.6);
+    naturalLogarithmOfHeightsMaxima.fill(8.01);
+    linewidthsMinima.fill(0.01);
+    linewidthsMaxima.fill(0.5);
+
+*/
+
+    parametersMinima.segment(0, Nmodes) = centralFrequencyMinima;
+    parametersMinima.segment(Nmodes, Nmodes) = naturalLogarithmOfHeightsMinima;
+    parametersMinima.segment(2*Nmodes, Nmodes) = linewidthsMinima;
+    parametersMaxima.segment(0, Nmodes) = centralFrequencyMaxima;
+    parametersMaxima.segment(Nmodes, Nmodes) = naturalLogarithmOfHeightsMaxima;
+    parametersMaxima.segment(2*Nmodes, Nmodes) = linewidthsMaxima;
 
     UniformPrior uniformPrior(parametersMinima, parametersMaxima);
     ptrPriors[0] = &uniformPrior;
-    //*/
-
-
-    // Normal Prior
-    /*
-
-    ArrayXd parametersMean(Ndimensions);                    // Mean
-    
-    double noiseLevelMean = 5;                              // Flat noise level
-    parametersMean(0) = noiseLevelMean;
-
-    ArrayXd centralFrequencyMean(Nmodes);                   // Central frequency of oscillation mode
-    centralFrequencyMean(0) = 200.0;
-    
-    ArrayXd naturalLogarithmOfHeightsMean(Nmodes);          // Natural logarithm of mode height
-    naturalLogarithmOfHeightsMean.fill(4.6);
-    
-    ArrayXd linewidthsMean(Nmodes);                         // Mode Linewidth (FWHM)
-    linewidthsMean.fill(0.05);
-    
-    parametersMean.segment(NglobalParameters, Nmodes) = centralFrequencyMean;
-    parametersMean.segment(NglobalParameters + Nmodes, Nmodes) = naturalLogarithmOfHeightsMean;
-    parametersMean.segment(NglobalParameters + 2*Nmodes, Nmodes) = linewidthsMean;
     
 
-    ArrayXd parametersSDV(Ndimensions);                     // SDV
-    
-    double noiseLevelSDV = 20;                              // Flat noise level
-    parametersSDV(0) = noiseLevelSDV;
-
-    ArrayXd centralFrequencySDV(Nmodes);                    // Central frequency of oscillation mode
-    centralFrequencySDV(0) = 201.0;
-    
-    ArrayXd naturalLogarithmOfHeightsSDV(Nmodes);           // Natural logarithm of mode height
-    naturalLogarithmOfHeightsSDV.fill(8.01);
-    
-    ArrayXd linewidthsSDV(Nmodes);                          // Mode Linewidth (FWHM)
-    linewidthsSDV.fill(0.5);
-    
-    parametersSDV.segment(NglobalParameters, Nmodes) = centralFrequencySDV;
-    parametersSDV.segment(NglobalParameters + Nmodes, Nmodes) = naturalLogarithmOfHeightsSDV;
-    parametersSDV.segment(NglobalParameters + 2*Nmodes, Nmodes) = linewidthsSDV;
-
-    NormalPrior normalPrior(parametersMean, parametersSDV);
-    ptrPriors[0] = &normalPrior;
-
-    
-    // Super-Gaussian Prior
-    ArrayXd parametersWOP(Ndimensions);                     // WOP
-    
-    double noiseLevelWOP = 20;                              // Flat noise level
-    parametersWOP(0) = noiseLevelWOP;
-
-    ArrayXd centralFrequencyWOP(Nmodes);                    // Central frequency of oscillation mode
-    centralFrequencyWOP(0) = 201.0;
-    
-    ArrayXd naturalLogarithmOfHeightsWOP(Nmodes);           // Natural logarithm of mode height
-    naturalLogarithmOfHeightsWOP.fill(8.01);
-    
-    ArrayXd linewidthsWOP(Nmodes);                          // Mode Linewidth (FWHM)
-    linewidthsWOP.fill(0.5);
-    
-    parametersWOP.segment(NglobalParameters, Nmodes) = centralFrequencyWOP;
-    parametersWOP.segment(NglobalParameters + Nmodes, Nmodes) = naturalLogarithmOfHeightsWOP;
-    parametersWOP.segment(NglobalParameters + 2*Nmodes, Nmodes) = linewidthsWOP;
-    
-    SuperGaussianPrior superGaussianPrior(parametersMean, parametersSDV, parametersWOP);
-    ptrPriors[0] = &superGaussianPrior;
-*/
-    
-
-    // Third step - Set up the likelihood function to be used
+    // -----------------------------------------------------------------
+    // ----- Third step. Set up the likelihood function to be used -----
+    // -----------------------------------------------------------------
     
     ExponentialLikelihood likelihood(observations, model);
     
 
-    // Fourth step - Set up the K-means clusterer using an Euclidean metric
+    // -------------------------------------------------------------------------------
+    // ----- Fourth step. Set up the K-means clusterer using an Euclidean metric -----
+    // -------------------------------------------------------------------------------
 
     EuclideanMetric myMetric;
     int minNclusters = 1;
@@ -249,11 +197,13 @@ int main(int argc, char *argv[])
     KmeansClusterer kmeans(myMetric, minNclusters, maxNclusters, Ntrials, relTolerance); 
 
 
-    // Start nested sampling process
+    // ---------------------------------------------------------------------
+    // ----- Sixth step. Configure and start nested sampling inference -----
+    // ---------------------------------------------------------------------
     
     bool printOnTheScreen = true;                   // Print results on the screen
-    int initialNobjects = 3000;                     // Maximum (and initial) number of live points evolving within the nested sampling process. 
-    int minNobjects = 3000;                          // Minimum number of live points allowed in the computation
+    int initialNobjects = 1000;                     // Maximum (and initial) number of live points evolving within the nested sampling process. 
+    int minNobjects = 1000;                          // Minimum number of live points allowed in the computation
     int maxNdrawAttempts = 10000;                   // Maximum number of attempts when trying to draw a new sampling point
     int NinitialIterationsWithoutClustering = static_cast<int>(initialNobjects*0.5);    // The first N iterations, we assume that there is only 1 cluster
     int NiterationsWithSameClustering = static_cast<int>(initialNobjects*0.05);        // Clustering is only happening every N iterations.
@@ -267,25 +217,27 @@ int main(int argc, char *argv[])
     
     // Save configuring parameters into an ASCII file
 
-    ofstream outputFile;
-    string outputDirName(argv[2]);
+    ofstream outputFile; 
     string fullPath = outputDirName + "peakBagging_configuringParameters.txt";
     File::openOutputFile(outputFile, fullPath);
     File::configuringParametersToFile(outputFile, initialNobjects, minNobjects, minNclusters, maxNclusters, NinitialIterationsWithoutClustering,
                                      NiterationsWithSameClustering, maxNdrawAttempts, initialEnlargementFraction, shrinkingRate, terminationFactor);
     outputFile.close();
 
-
     MultiEllipsoidSampler nestedSampler(printOnTheScreen, ptrPriors, likelihood, myMetric, kmeans, 
                                         initialNobjects, minNobjects, initialEnlargementFraction, shrinkingRate);
 
+
+    // Choose which reducer of the live points to adopt and include it in the run.
+
     double toleranceOnEvidence = 0.01;
     FerozReducer livePointsReducer(nestedSampler, toleranceOnEvidence);
-
     nestedSampler.run(livePointsReducer, terminationFactor, NinitialIterationsWithoutClustering, NiterationsWithSameClustering, maxNdrawAttempts);
 
 
-    // Save the results in output files
+    // -------------------------------------------------------
+    // ----- Last step. Save the results in output files -----
+    // -------------------------------------------------------
     
     Results results(nestedSampler);
     results.writeParametersToFile(outputDirName + "parameter");
@@ -293,6 +245,9 @@ int main(int argc, char *argv[])
     results.writeEvidenceInformationToFile(outputDirName + "evidenceInformation.txt");
     results.writePosteriorProbabilityToFile(outputDirName + "posteriorDistribution.txt");
     results.writeParametersSummaryToFile(outputDirName + "parameterSummary.txt");
+
+    
+    // END
 
     return EXIT_SUCCESS;
 }
