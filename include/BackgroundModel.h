d9175db426e83f77d9492ef277bcc798de80adcc
Enrico Corsaro <enrico.corsaro@ster.kuleuven.be>
2013-11-21 23:01:45 +0100

    Added new derived abstract class BackgroundModel for any model that is able to predict the background level in RG stars.

---
+++ include/BackgroundModel.h
@@ -0,0 +1,45 @@
+// Derived abstract class for global background fit to RG stars.
+// Created by Enrico Corsaro @ IvS - 21 November 2013
+// e-mail: enrico.corsaro@ster.kuleuven.be
+// Header file "BackgroundModel.h"
+// Implementations contained in "BackgroundModel.cpp"
+
+
+#ifndef BACKGROUNDMODEL_H
+#define BACKGROUNDMODEL_H
+
+#include <iostream>
+#include "Model.h"
+#include "Functions.h"
+#include "File.h"
+
+using namespace std;
+using Eigen::ArrayXd;
+typedef Eigen::Ref<Eigen::ArrayXd> RefArrayXd;
+
+
+class BackgroundModel : public Model
+{
+    public:
+    
+        BackgroundModel(const RefArrayXd covariates);
+        ~BackgroundModel();
+
+        void readConfiguringParametersFromFile(const string inputFileName);
+        ArrayXd getConfiguringParameters();
+
+        virtual void predict(RefArrayXd predictions, RefArrayXd const modelParameters) = 0;
+        virtual void predict(RefArrayXd predictions) = 0;
+
+
+    protected:
+
+        ArrayXd configuringParameters;
+
+
+    private:
+
+}; 
+
+
+#endif
---
+++ source/BackgroundModel.cpp
@@ -0,0 +1,104 @@
+#include "BackgroundModel.h"
+
+
+// BackgroundModel::BackgroundModel()
+//
+// PURPOSE: 
+//      Constructor. Initializes model computation.
+//
+// INPUT:
+//      covariates:             one-dimensional array containing the values
+//                              of the independent variable.
+//      NparametersPerType:     configuring numbers for the Peak Bagging model
+//      nuMax:                  the frequency of maximum power excess
+//
+
+BackgroundModel::BackgroundModel(const RefArrayXd covariates)
+: Model(covariates)
+{
+}
+
+
+
+
+
+
+
+
+
+
+// BackgroundModel::BackgroundModel()
+//
+// PURPOSE: 
+//      Destructor.
+//
+
+BackgroundModel::~BackgroundModel()
+{
+
+}
+
+
+
+
+
+
+
+
+
+
+
+// BackgroundModel::readConfiguringParametersFromFile()
+//
+// PURPOSE:
+//      Reads the configuring parameters of the model from an input file
+//      specified by its full path as a string. The values are then
+//      store into a vector which is a protected data member and that can
+//      be therefore accessed from a derived class that implements the model.
+//
+// INPUT:
+//      inputFileName:      a string specifying the full path (filename included) of the input file to read.
+//
+// OUTPUT:
+//      void
+//
+
+void BackgroundModel::readConfiguringParametersFromFile(const string inputFileName)
+{
+    ifstream inputFile;
+    File::openInputFile(inputFile, inputFileName);
+
+    unsigned long Nrows;
+    int Ncols;
+
+    File::sniffFile(inputFile, Nrows, Ncols);
+    
+    configuringParameters.resize(Nrows);
+    configuringParameters = File::arrayXXdFromFile(inputFile, Nrows, Ncols);
+
+    inputFile.close();
+}
+
+
+
+
+
+
+
+
+
+
+// BackgroundModel::getConfiguringParameters()
+//
+// PURPOSE:
+//      Gets the protected data member configuringParameters.
+//
+// OUTPUT:
+//      An eigen array containing the configuring parameters of the
+//      background model.
+//
+
+ArrayXd BackgroundModel::getConfiguringParameters()
+{
+    return configuringParameters;
+}
