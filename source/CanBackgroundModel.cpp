0c715cb219d63fa21d9efe5df1d3e6d81eae9930
Enrico Corsaro <enrico.corsaro@ster.kuleuven.be>
2014-01-23 14:16:30 +0100

    Updated CAN background model for red giant stars with apodization.

--- source/CanBackgroundModel.cpp
+++ source/CanBackgroundModel.cpp
@@ -14,6 +14,11 @@
 CanBackgroundModel::CanBackgroundModel(const RefArrayXd covariates)
 : BackgroundModel(covariates)
 {
+    // Create response function modulating the sampling rate of input Kepler LC data
+
+    double NyquistFrequency = 283.2116656017908;    // muHz
+    ArrayXd sincFunctionArgument = (Functions::PI / 2.0) * covariates / NyquistFrequency;
+    responseFunction = (sincFunctionArgument.sin() / sincFunctionArgument).square(); 
 }
 
 
@@ -70,24 +75,39 @@ void CanBackgroundModel::predict(RefArrayXd predictions)
     // Initialize global parameters
 
     double flatNoiseLevel = configuringParameters(0);
-    double amplitude1 = configuringParameters(1);
-    double amplitude2 = configuringParameters(2);
-    double amplitude3 = configuringParameters(3);
-    double noiseFrequency1 = configuringParameters(4);
-    double noiseFrequency2 = configuringParameters(5);
-    double noiseFrequency3 = configuringParameters(6);
+    double amplitudeHarvey1 = configuringParameters(1);
+    double amplitudeHarvey2 = configuringParameters(2);
+    double amplitudeHarvey3 = configuringParameters(3);
+    double frequencyHarvey1 = configuringParameters(4);
+    double frequencyHarvey2 = configuringParameters(5);
+    double frequencyHarvey3 = configuringParameters(6);
+    double heightOscillation = configuringParameters(7);
+    double nuMax = configuringParameters(8);
+    double sigma = configuringParameters(9);
 
 
-    // Compute power law components and add them to the predictions
+    // Compute Harvey components and add them to the predictions
 
-    predictions = 2*Functions::PI*amplitude1*amplitude1/(noiseFrequency1*(1.0 + pow(covariates/noiseFrequency1,4)));
-    predictions += 2*Functions::PI*amplitude2*amplitude2/(noiseFrequency2*(1.0 + pow(covariates/noiseFrequency2,4)));
-    predictions += 2*Functions::PI*amplitude3*amplitude3/(noiseFrequency3*(1.0 + pow(covariates/noiseFrequency3,4)));
+    predictions = 2*Functions::PI*amplitudeHarvey1*amplitudeHarvey1/(frequencyHarvey1*(1.0 + (covariates/frequencyHarvey1).pow(4)));
+    predictions += 2*Functions::PI*amplitudeHarvey2*amplitudeHarvey2/(frequencyHarvey2*(1.0 + (covariates/frequencyHarvey2).pow(4)));
+    predictions += 2*Functions::PI*amplitudeHarvey3*amplitudeHarvey3/(frequencyHarvey3*(1.0 + (covariates/frequencyHarvey3).pow(4)));
+
+
+    // Compute Gaussian envelope for RGB Oscillations and add it to the predictions
+
+    predictions += heightOscillation * exp(-1.0*(nuMax - covariates)*(nuMax - covariates)/(2.0 * sigma * sigma));
+    
+    
+    // Modulate the model by the response function (apodization)
+    
+    predictions *= responseFunction;        
 
 
     // Add flat noise level component
 
-    predictions += flatNoiseLevel;           
+    predictions += flatNoiseLevel;
+
+    
 }
 
 
