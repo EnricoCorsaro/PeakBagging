d8555ba18ffec0ecace129ac1afcc802ad29aa1e
Enrico Corsaro <emncorsaro@gmail.com>
2019-07-21 12:55:01 -0400

    Updated the LorentzianRotationMixture model. This model can now run a fit over multiple peaks and at the same time fit cos i and rotational splitting for each mode with an angular degree > 0. The rotational split components are treated according to the modulation of visibilities presented by Gizon & Solanki (2003).

--- include/LorentzianRotationMixtureModel.h
+++ include/LorentzianRotationMixtureModel.h
@@ -20,13 +20,11 @@ class LorentzianRotationMixtureModel : public Model
 {
     public:
     
-        LorentzianRotationMixtureModel(RefArrayXd const covariates, const int NmodesWithRotation, const int NmodesWithoutRotation, 
-                                       const double cosi, string angularDegreesFileName, BackgroundModel &backgroundModel); 
+        LorentzianRotationMixtureModel(RefArrayXd const covariates, const int Nresolved, string angularDegreesFileName, BackgroundModel &backgroundModel); 
         ~LorentzianRotationMixtureModel();
 
         virtual void predict(RefArrayXd predictions, RefArrayXd const modelParameters);
-        void computeModeVisibility();
-        void createModeAmplitudeVisibility();
+        ArrayXd computeModeVisibility(const int angularDegree, const double cosi);
 
 
     protected:
@@ -36,22 +34,12 @@ class LorentzianRotationMixtureModel : public Model
 
     private:
 
-        int NmodesWithRotation;                 // Total number of p modes in which the rotational effect is fitted
-        int NmodesWithoutRotation;              // Total number of p modes in which the rotational effect is not fitted
-        double cosi;                            // Solar rotation axis inclination angle
-        string angularDegreesFileName;          // a string containing the file name of the ASCII file with the list of angular degrees for the modes
-                                                // where rotation has to be fitted
+        int Nresolved;                          // Total number modes considered in the fit.
+        string angularDegreesFileName;          // a string containing the file name of the ASCII file with the list of angular degrees for all
+                                                // the modes considered in the fit.
         ArrayXd backgroundPrediction;           // An array containing the prediction for the background in all the range of the covariates
         ArrayXd responseFunction;               // An array containing the apodization response function for the signal of the input data
-        ArrayXd backgroundParameters;           // An array containing the configuring parameters for the background model adopted
         ArrayXd angularDegreeArray;             // An array of dimension Nresolved+Nunresolved containing the angular degree of the mode
-                                                // The elements must be sorted in the same order as the modes are defined in the prior list
-        ArrayXd modeAmplitudeVisibilityArray;   // The visibility rescaled for the amplitude for each angular degree and azimuthal component 
-                                                // following the input order of the modes with rotation
-        ArrayXd modeVisibility1;                // Mode visibility for l=1 modes
-        ArrayXd modeVisibility2;                // Mode visibility for l=2 modes
-        ArrayXd modeVisibility3;                // Mode visibility for l=3 modes
-        ArrayXd modeVisibility4;                // Mode visibility for l=4 modes
 }; 
 
 
--- source/LorentzianRotationMixtureModel.cpp
+++ source/LorentzianRotationMixtureModel.cpp
@@ -9,39 +9,29 @@
 // INPUT:
 //      covariates:             one-dimensional array containing the values
 //                              of the independent variable.
-//      NnonRadial:             the number of non radial p modes (to be fitted by a Lorentzian profile)
-//      Nradial:                the number of radial modes (to be fitted by a Lorentzian profile)
-//      cosi:                   the cosine of the inclination angle for the Sun (i = 90.0 degrees by default)
-//      frequencyResolution:    the frequency bin size given by the dataset used
+//      angularDegreesFileName: a string containing the filename of the input list of angular degrees, one
+//                              for each mode considered in the fit.
+//      Nresolved:              total number of modes.
 //      backgroundModel:        an object of class BackgroundModel containing a
 //                              model for the background of the star.
 //
 
-LorentzianRotationMixtureModel::LorentzianRotationMixtureModel(RefArrayXd const covariates, const int NmodesWithRotation, 
-                                                               const int NmodesWithoutRotation, const double cosi, 
+LorentzianRotationMixtureModel::LorentzianRotationMixtureModel(RefArrayXd const covariates, const int Nresolved, 
                                                                string angularDegreesFileName, BackgroundModel &backgroundModel)
 : Model(covariates),
-  NmodesWithRotation(NmodesWithRotation),
-  NmodesWithoutRotation(NmodesWithoutRotation),
-  cosi(cosi),
+  Nresolved(Nresolved),
   angularDegreesFileName(angularDegreesFileName)
 {
     backgroundPrediction.resize(covariates.size());
     backgroundModel.predict(backgroundPrediction);
     responseFunction = backgroundModel.getResponseFunction();
-    backgroundParameters = backgroundModel.getConfiguringParameters();
 
 
     // Read the angular degrees of the modes with rotational signature to be fitted
 
     readAngularDegreesFromFile(angularDegreesFileName);
-
-    
-    // Compute the mode visibilities for each angular degree and azimuthal number using the cosi given as input
-
-    computeModeVisibility(); 
 }
-
+    
 
 
 
@@ -92,76 +82,67 @@ LorentzianRotationMixtureModel::~LorentzianRotationMixtureModel()
 //      (i) Mode central frequency (times the number of resolved peaks)
 //      (ii) Mode profile amplitude (times the number of resolved peaks)
 //      (iii) Mode profile linewidth (times the number of resolved peaks)
-//      (iv) Mode central frequency (times the number of unresolved peaks)
-//      (v) Mode profile height (times the number of unresolved peaks)
+//      (iv) rotational splitting
+//      (v) cos i, with i the inclination angle of the rotation axis
 //
 
 void LorentzianRotationMixtureModel::predict(RefArrayXd predictions, RefArrayXd const modelParameters)
 {
     ArrayXd singleModePrediction = ArrayXd::Zero(covariates.size());
+    double rotationalSplitting = modelParameters(modelParameters.size() - 2);
+    double cosi = modelParameters(modelParameters.size() - 1);
 
+    // Add a Lorentzian profile with split components for each mode for which the rotational effect is fitted (only l=1 for this model)
 
-    // Add a Lorentzian profile with split components for each mode for which the rotational effect is fitted
-
-    for (int mode = 0; mode < NmodesWithRotation; ++mode)
+    for (int mode = 0; mode < Nresolved; ++mode)
     {
         // Initialize parameters of current mode with proper access to elements of total array of free parameters
 
-        double centralFrequency = modelParameters(4*mode);
-        double amplitude = modelParameters(4*mode + 1);
-        double linewidth = modelParameters(4*mode + 2);
-        double rotationalSplitting = modelParameters(4*mode + 3);
+        double centralFrequency = modelParameters(3*mode);
+        double amplitude = modelParameters(3*mode + 1);
+        double linewidth = modelParameters(3*mode + 2);
 
         
-        // Obtain the angular degree of this mode and find the corresponding starting element index in the visibility array
+        // Obtain the angular degree of this mode
         
         double angularDegree = angularDegreeArray(mode);
-        int startingIndex = static_cast<int>(angularDegree*(angularDegree - 1)/2 + angularDegree - 1);
-   
-        
-        // Include first the m=0 component
-
-        double amplitudeVisibilityComponent = modeAmplitudeVisibilityArray(startingIndex);
-        Functions::modeProfileWithAmplitude(singleModePrediction, covariates, centralFrequency, 
-                                            amplitude*amplitudeVisibilityComponent, linewidth);
-        predictions += singleModePrediction;
-
-        
-        // Then loop over the remaining 2*l components
-
-        for (int azimuthalNumber = 1; azimuthalNumber <= angularDegree; azimuthalNumber++)
+        ArrayXd modeAmplitudeVisibility = computeModeVisibility(angularDegree, cosi);
+  
+        if (angularDegree != 0)
         {
-            amplitudeVisibilityComponent = modeAmplitudeVisibilityArray(startingIndex + azimuthalNumber);       // m != 0
-            
-            // + m
-            Functions::modeProfileWithAmplitude(singleModePrediction, covariates, centralFrequency + (rotationalSplitting*azimuthalNumber), 
-                                                amplitude*amplitudeVisibilityComponent, linewidth);
+            // Include first the m=0 component
+
+            Functions::modeProfileWithAmplitude(singleModePrediction, covariates, centralFrequency, 
+                                                amplitude*modeAmplitudeVisibility(0), linewidth);
             predictions += singleModePrediction;
+
             
-            // - m
-            Functions::modeProfileWithAmplitude(singleModePrediction, covariates, centralFrequency - (rotationalSplitting*azimuthalNumber), 
-                                                amplitude*amplitudeVisibilityComponent, linewidth);
+            // Then loop over the remaining 2*ell components
+
+            for (int azimuthalNumber = 1; azimuthalNumber <= angularDegree; azimuthalNumber++)
+            {
+                // + m
+                Functions::modeProfileWithAmplitude(singleModePrediction, covariates, centralFrequency + (rotationalSplitting*azimuthalNumber), 
+                                                    amplitude*modeAmplitudeVisibility(azimuthalNumber), linewidth);
+                predictions += singleModePrediction;
+                
+                // - m
+                Functions::modeProfileWithAmplitude(singleModePrediction, covariates, centralFrequency - (rotationalSplitting*azimuthalNumber), 
+                                                    amplitude*modeAmplitudeVisibility(azimuthalNumber), linewidth);
+                predictions += singleModePrediction;
+            }
+        }
+        else
+        {
+            // Add a Lorentzian profile for each mode for which no rotational effect is fitted
+            
+            Functions::modeProfileWithAmplitude(singleModePrediction, covariates, centralFrequency, amplitude, linewidth);
             predictions += singleModePrediction;
         }
         
     }
 
 
-    // Add a Lorentzian profile for each mode for which no rotational effect is fitted
-
-    for (int mode = 0; mode < NmodesWithoutRotation; ++mode)
-    {
-        // Initialize parameters of current mode with proper access to elements of total array of free parameters
-
-        double centralFrequency = modelParameters(4*NmodesWithRotation + 3*mode);
-        double amplitude = modelParameters(4*NmodesWithRotation + 3*mode + 1);
-        double linewidth = modelParameters(4*NmodesWithRotation + 3*mode + 2);
-
-        Functions::modeProfileWithAmplitude(singleModePrediction, covariates, centralFrequency, amplitude, linewidth);
-        predictions += singleModePrediction;
-    }
-
-    
     // Adjust by apodization of the signal
     
     predictions *= responseFunction;
@@ -210,12 +191,7 @@ void LorentzianRotationMixtureModel::readAngularDegreesFromFile(const string inp
 
     inputFile.close();
 
-    if (angularDegreeArray.size() != NmodesWithRotation)
-    {
-        cerr << "The number of input angular degrees is not matching" << endl;
-        cerr << "the number of non radial modes in the fit. Quitting program." << endl;
-    }
-
+    assert(angularDegreeArray.size() == Nresolved);
 }
 
 
@@ -233,143 +209,97 @@ void LorentzianRotationMixtureModel::readAngularDegreesFromFile(const string inp
 //
 // PURPOSE: 
 //      Computes the visibilities of each split component using the cosine of the 
-//      inclination angle defined by the constructor.
+//      inclination angle as input and the corresponding angular degree of interest.
 //
 // OUTPUT:
-//      void
+//      An array of mode visibilities for amplitudes (hence original mode visibilities under squareroot).
 //
 
-void LorentzianRotationMixtureModel::computeModeVisibility()
+ArrayXd LorentzianRotationMixtureModel::computeModeVisibility(const int angularDegree, const double cosi)
 {
     double modeVisibilityComponent;
-    modeVisibility1.resize(2);
-    modeVisibility2.resize(3);
-    modeVisibility3.resize(4);
-    modeVisibility4.resize(5);
-    
+    ArrayXd modeVisibility; 
 
-    // l = 1    m = 0
-    modeVisibilityComponent = cosi*cosi;
-    modeVisibility1(0) = modeVisibilityComponent;
-            
-    // l = 1    m = -1 / +1
-    modeVisibilityComponent = 0.5*(1.0 - cosi*cosi);
-    modeVisibility1(1) = modeVisibilityComponent;
-            
-
-    // l = 2    m = 0
-    modeVisibilityComponent = 0.25*pow((3*cosi*cosi - 1.0),2);
-    modeVisibility2(0) = modeVisibilityComponent;
-            
-    // l = 2    m = -1 / + 1
-    modeVisibilityComponent = (3.0/2.0)*cosi*cosi*(1.0 - cosi*cosi); 
-    modeVisibility2(1) = modeVisibilityComponent;
-            
-    // l = 2    m = -2 / + 2
-    modeVisibilityComponent = (3.0/8.0)*pow((1.0 - cosi*cosi),2); 
-    modeVisibility2(2) = modeVisibilityComponent;
-            
-            
-    // l = 3    m = 0
-    modeVisibilityComponent = 0.25*cosi*cosi*pow((5.0*cosi*cosi - 3.0),2);
-    modeVisibility3(0) = modeVisibilityComponent;
-            
-    // l = 3    m = -1 / +1
-    modeVisibilityComponent = (1.0/12.0)*(9.0/4.0)*pow((1.0 - 5.0*cosi*cosi),2)*(1.0 - cosi*cosi);
-    modeVisibility3(1) = modeVisibilityComponent;
-            
-    // l = 3    m = -2 / +2
-    modeVisibilityComponent = 1.87500*cosi*cosi*pow((1.0 - cosi*cosi),2);
-    modeVisibility3(2) = modeVisibilityComponent;
-            
-    // l = 3    m = -3 / +3
-    modeVisibilityComponent = 0.3125*pow((1.0 - cosi*cosi),3);
-    modeVisibility3(3) = modeVisibilityComponent;
-
-            
-    // l = 4    m = 0
-    modeVisibilityComponent = 0.0156250*pow((35*pow(cosi,4) - 30*cosi*cosi + 3),2);
-    modeVisibility4(0) = modeVisibilityComponent;
-                
-    // l = 4    m = -1 / +1
-    modeVisibilityComponent = 0.312500*cosi*cosi*pow((3.0 - 7.0*cosi*cosi),2)*(1 - cosi*cosi);
-    modeVisibility4(1) = modeVisibilityComponent;
-            
-    // l = 4    m = -2 / +2
-    modeVisibilityComponent = 0.156250*pow((7*cosi*cosi - 1.0)*(1.0 - cosi*cosi),2);
-    modeVisibility4(2) = modeVisibilityComponent;
-            
-    // l = 4    m = -3 / +3
-    modeVisibilityComponent = 2.1875000*cosi*cosi*pow((1.0 - cosi*cosi),3);
-    modeVisibility4(3) = modeVisibilityComponent;
-
-    // l = 4    m = -4 / +4
-    modeVisibilityComponent = 0.27343750*pow((1.0 - cosi*cosi),4);
-    modeVisibility4(4) = modeVisibilityComponent;
-
-    
-    // Convert these visibilities into amplitude visibilities
-
-    createModeAmplitudeVisibility();
-}
-
-
-
-
-
-
-
-
-
-
-
-// LorentzianRotationMixtureModel::createModeAmplitudeVisibility()
-//
-// PURPOSE: 
-//      Stores the visibilities in amplitude for each angular degree and 
-//      azimuthal number as obtained from the angularDegreeArray.
-//
-// OUTPUT:
-//      void
-//
-
-void LorentzianRotationMixtureModel::createModeAmplitudeVisibility()
-{
-    int currentAngularDegree;
-    int currentSize = 0;
-
-    for (int i = 0; i < NmodesWithRotation; i++)
+    if (angularDegree == 1)
     {
-        currentAngularDegree = static_cast<int>(angularDegreeArray(i));
+        modeVisibility.resize(2); 
+        
+        // l = 1    m = 0
+        modeVisibilityComponent = cosi*cosi;
+        modeVisibility(0) = modeVisibilityComponent;
+                
+        // l = 1    m = -1 / +1
+        modeVisibilityComponent = 0.5*(1.0 - cosi*cosi);
+        modeVisibility(1) = modeVisibilityComponent;
 
-        switch(currentAngularDegree)
-        {
-            case(1):          // l = 1 modes
-                modeAmplitudeVisibilityArray.conservativeResize(currentSize + 2);
-                modeAmplitudeVisibilityArray.segment(currentSize, 2) = modeVisibility1.sqrt();
-                break;
+    }       
 
-            case(2):          // l = 2 modes
-                modeAmplitudeVisibilityArray.conservativeResize(currentSize + 3);
-                modeAmplitudeVisibilityArray.segment(currentSize, 3) = modeVisibility2.sqrt();
-                break;
-
-            case(3):          // l = 3 modes
-                modeAmplitudeVisibilityArray.conservativeResize(currentSize + 4);
-                modeAmplitudeVisibilityArray.segment(currentSize, 4) = modeVisibility3.sqrt();
-                break;
-
-            case(4):          // l = 4 modes
-                modeAmplitudeVisibilityArray.conservativeResize(currentSize + 5);
-                modeAmplitudeVisibilityArray.segment(currentSize, 5) = modeVisibility4.sqrt();
-                break;
-        }
-       
-
-        // Update the number of elements of the modeAmplitudeVisibilityArray for the next iteration
-
-        currentSize = modeAmplitudeVisibilityArray.size();
-    }
+    if (angularDegree == 2)
+    {
+        modeVisibility.resize(3); 
+        
+        // l = 2    m = 0
+        modeVisibilityComponent = 0.25*pow((3*cosi*cosi - 1.0),2);
+        modeVisibility(0) = modeVisibilityComponent;
+                
+        // l = 2    m = -1 / + 1
+        modeVisibilityComponent = (3.0/2.0)*cosi*cosi*(1.0 - cosi*cosi); 
+        modeVisibility(1) = modeVisibilityComponent;
+                
+        // l = 2    m = -2 / + 2
+        modeVisibilityComponent = (3.0/8.0)*pow((1.0 - cosi*cosi),2); 
+        modeVisibility(2) = modeVisibilityComponent;
+        
+    }       
     
+    if (angularDegree == 3)
+    {
+        modeVisibility.resize(4); 
+        
+        // l = 3    m = 0
+        modeVisibilityComponent = 0.25*cosi*cosi*pow((5.0*cosi*cosi - 3.0),2);
+        modeVisibility(0) = modeVisibilityComponent;
+                
+        // l = 3    m = -1 / +1
+        modeVisibilityComponent = (1.0/12.0)*(9.0/4.0)*pow((1.0 - 5.0*cosi*cosi),2)*(1.0 - cosi*cosi);
+        modeVisibility(1) = modeVisibilityComponent;
+                
+        // l = 3    m = -2 / +2
+        modeVisibilityComponent = 1.87500*cosi*cosi*pow((1.0 - cosi*cosi),2);
+        modeVisibility(2) = modeVisibilityComponent;
+                
+        // l = 3    m = -3 / +3
+        modeVisibilityComponent = 0.3125*pow((1.0 - cosi*cosi),3);
+        modeVisibility(3) = modeVisibilityComponent;
+        
+    }
+     
+    if (angularDegree == 4)
+    {
+        modeVisibility.resize(5);
 
+        // l = 4    m = 0
+        modeVisibilityComponent = 0.0156250*pow((35*pow(cosi,4) - 30*cosi*cosi + 3),2);
+        modeVisibility(0) = modeVisibilityComponent;
+                    
+        // l = 4    m = -1 / +1
+        modeVisibilityComponent = 0.312500*cosi*cosi*pow((3.0 - 7.0*cosi*cosi),2)*(1.0 - cosi*cosi);
+        modeVisibility(1) = modeVisibilityComponent;
+                
+        // l = 4    m = -2 / +2
+        modeVisibilityComponent = 0.156250*pow((7*cosi*cosi - 1.0)*(1.0 - cosi*cosi),2);
+        modeVisibility(2) = modeVisibilityComponent;
+                
+        // l = 4    m = -3 / +3
+        modeVisibilityComponent = 2.1875000*cosi*cosi*pow((1.0 - cosi*cosi),3);
+        modeVisibility(3) = modeVisibilityComponent;
+
+        // l = 4    m = -4 / +4
+        modeVisibilityComponent = 0.27343750*pow((1.0 - cosi*cosi),4);
+        modeVisibility(4) = modeVisibilityComponent;
+        
+    }
+        
+    return modeVisibility.sqrt();
 }
+
